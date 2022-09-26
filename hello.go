e984be5bad0b0384920a3a3ea4299ea18cb2d1c4
NoMoreZer0 <kazakupersuper@mail.ru>
2022-09-26 13:09:21 +0600

    test

---
+++ hello.go
@@ -0,0 +1,85 @@
+package main
+
+/*
+	Posidet che takoe prochitat OOP
+
+	Program to an interface, not an implementation
+
+	Prefer composition over inheritance
+
+	1. Strategy Pattern
+			Sdelat snachala duck class
+			Sdelat interface dlya performFly
+			Sdelat dva otdelnix classa NoFly i MakeFly dlya letayushix i ne letayushix utok
+			Napisat vnutri kazhdogo otdelnogo duck a interface
+			Potom prost sdelat settor togo ili inogo classa interfaca dlya kazhdogo duck a
+
+
+	Data is a crud oil
+
+	new model: everybody can generate some data
+	old model: few companies generating data all others are consuming
+
+	Big Data can be analyzed to gather some insights that lead to better decisions and strategic business moves
+
+
+	Data generation stages
+		1. Opeartion and business system
+			data is generated as a business activity (early passive)
+		2. User-generated contant
+			blogs, social media; mobile phones (active)
+		3. Perception stage
+			Sensor with processing capability,automatic; (7 * 24) This is the main reason for big data
+
+
+	Four major technologies empower:
+		- Big Data
+		- AI
+		- IoT
+		- 5G
+
+	only 20% data is structured and 80% is unstructured
+
+	Structured Data
+		- Can be displated in relational databases
+		- Number, dates, string
+		- Require less storage
+		- Easier to manage and protect
+
+	Unstructed Data
+		- Cannot be displated in rows and columns
+		- Images, audio, video
+		- Requires more storage
+		- More difficult to manageand protect with legacy solutions
+
+	Structured Data
+	Semi Structured Data -> some data with patterns
+	Quasi Structured Data -> textual data has erratic formatcs. E.g clickstream Data
+	Unstructured
+
+	in Big Data your first have the Data. And then you think of schema to store the Data
+
+	Jim Gray - a chel who created relationl databases
+
+	3v - volume, velocity, variety
+
+	volume
+		- data scale increased 44 times from 2009 to 2040. From 0.8 zettabytes to ..
+
+	Earth Scope
+		more than 3.8 million squares of data and accumulating 67 tb data.
+
+	variety
+		- just the types of data. Text, Image, video etc
+
+	velocity
+		- Online Data Analytics
+		Late deicison -> missing opportunities
+
+	----------------------
+	AI it is try to mimic our approach.
+
+	Emergent behaviour - when a group behaves in ways that were not "programmed".
+
+	Swarm Intelligence - trying to simmulate social interaction of the humans.
+*/
